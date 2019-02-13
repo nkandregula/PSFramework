6a85a448e3580ac0f99e3675ea981ab26533fd44
Narayana Kandregula <narayana@psiblrs-imac.local>
2019-02-13 19:43:20 +0530

    Initial Commit

---
+++ PSFramework/PSFramework.h
@@ -0,0 +1,19 @@
+//
+//  PSFramework.h
+//  PSFramework
+//
+//  Created by Narayana rao Kandregula on 13/02/19.
+//  Copyright © 2019 Narayana rao Kandregula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PSFramework.
+FOUNDATION_EXPORT double PSFrameworkVersionNumber;
+
+//! Project version string for PSFramework.
+FOUNDATION_EXPORT const unsigned char PSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSFramework/PublicHeader.h>
+
+
